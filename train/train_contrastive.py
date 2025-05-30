8483d9aab1d0270f66ddb1e4f6154a61c0591f0e
mojmas <masoudi.m1991@gmail.com>
2025-05-30 13:18:35 +0100

    minimum 10 cap

--- train/train_contrastive.py
+++ train/train_contrastive.py
@@ -819,8 +819,8 @@ def train_cifar(rank, world_size, config, console):
                         new_proco_loss.estimator.kappa[node_id] = torch.tensor(kappa_j, device=device)
 
                         superclass_size = len(superclass_feats[sc_idx])
-                        # pseudo = max(int(pi_j * superclass_size), 100)
-                        pseudo = int(pi_j * superclass_size)
+                        pseudo = max(int(pi_j * superclass_size), 10)
+                        # pseudo = int(pi_j * superclass_size)
                         new_proco_loss.estimator.Amount[node_id] = pseudo
 
             ce_loss_all, scl_loss_all, top1 = train(epoch, train_loader, model, criterion_ce, new_criterion_scl,
