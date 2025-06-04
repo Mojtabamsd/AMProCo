b01654b5bc95882773a67eaf82645a884ac341bb
mojmas <masoudi.m1991@gmail.com>
2025-06-04 15:24:43 +0100

    remoce clamp

--- models/proco.py
+++ models/proco.py
@@ -322,12 +322,14 @@ class ProCoLoss(nn.Module):
 
         #new
         tau = torch.exp(self.log_tau).clamp(0.3, 3.0)
-        kappa_eff = (kappa / tau).clamp(1e-3, 1e5)
+        # kappa_eff = (kappa / tau).clamp(1e-3, 1e5)
+        kappa_eff = (kappa / tau)
 
         term = kappa_eff.reshape(-1, 1) * Ave_norm  # (K,D)
         T0 = self.temperature
         vec = features[:N].unsqueeze(1) / T0
-        kappa_new = torch.linalg.norm(term + vec, dim=2).clamp(1e-3, 1e5)
+        # kappa_new = torch.linalg.norm(term + vec, dim=2).clamp(1e-3, 1e5)
+        kappa_new = torch.linalg.norm(term + vec, dim=2)
 
         # tem = kappa.reshape(-1, 1) * Ave_norm
         # tem = tem.unsqueeze(0) + features[:N].unsqueeze(1) / self.temperature
