2901bdc45993b19902bb5f42211c37e28a8ddd5e
mojmas <masoudi.m1991@gmail.com>
2026-03-12 15:43:56 +0000

    to tensor

--- train/train_contrastive.py
+++ train/train_contrastive.py
@@ -584,7 +584,7 @@ def train_cifar(rank, world_size, config, console, torch_profile):
         transforms.RandomResizedCrop(config.training_contrastive.target_size[0]),
         # transforms.RandomHorizontalFlip(),
         # CIFAR10Policy(),
-        # transforms.ToTensor(),
+        transforms.ToTensor(),
         # Cutout(n_holes=1, length=16),
         # transforms.Normalize(
         #     (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
@@ -595,7 +595,7 @@ def train_cifar(rank, world_size, config, console, torch_profile):
         # transforms.RandomHorizontalFlip(p=0.5),
         # transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
         # transforms.RandomGrayscale(p=0.2),
-        # transforms.ToTensor(),
+        transforms.ToTensor(),
         # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]
 
