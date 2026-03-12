ccd3b2aa8a4189b640bb77b081704fbd7b7ed90c
tobias ferreira <soutobias@yahoo.com.br>
2026-03-12 14:26:28 +0000

    Profile all

--- train/train_contrastive.py
+++ train/train_contrastive.py
@@ -1486,5 +1486,6 @@ def make_profiler(torch_profile):
     return profile(
         activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
         record_shapes=True,
-        schedule=schedule(skip_first=3, wait=5, warmup=1, active=3, repeat=1),
+        profile_memory=True
+        # schedule=schedule(skip_first=3, wait=5, warmup=1, active=3, repeat=1),
     )
