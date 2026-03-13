85dac08bb4d4be83cf005cab8a919181449df425
tobias ferreira <soutobias@yahoo.com.br>
2026-03-13 12:02:28 +0000

    Add augmentation

--- train/train_contrastive.py
+++ train/train_contrastive.py
@@ -582,21 +582,21 @@ def train_cifar(rank, world_size, config, console, torch_profile):
     # Define data transformations
     augmentation_regular = [
         transforms.RandomResizedCrop(config.training_contrastive.target_size[0]),
-        # transforms.RandomHorizontalFlip(),
-        # CIFAR10Policy(),
+        transforms.RandomHorizontalFlip(),
+        CIFAR10Policy(),
         transforms.ToTensor(),
-        # Cutout(n_holes=1, length=16),
-        # transforms.Normalize(
-        #     (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        Cutout(n_holes=1, length=16),
+        transforms.Normalize(
+            (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         ]
 
     augmentation_sim_cifar = [
         transforms.RandomResizedCrop(size=config.training_contrastive.target_size[0]),
-        # transforms.RandomHorizontalFlip(p=0.5),
-        # transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
-        # transforms.RandomGrayscale(p=0.2),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+        transforms.RandomGrayscale(p=0.2),
         transforms.ToTensor(),
-        # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ]
 
     transform_train = [transforms.Compose(augmentation_regular),
